32033397a022d766df76972bef8f2f39df4798eb
kooky-optimization-7340 <christinafox93@gmail.com>
2019-03-21 16:25:41 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,6 +1,6 @@
 # code your #valid_move? method here
 def valid_move(board,index)
-  if position_taken? = false
+  if position_taken?(board,index) == false
     if index >= 1 && index <= 9
       true
   else
